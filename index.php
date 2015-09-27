73e734abc2d673ed3764e7bb969be66d134a2765
Roberto <robertooliveiraestudo@gmail.com>
2015-09-27 19:38:47 -0300

    First commit.

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>R0 Dash</title>
+	<!-- Font -->
+	<link href='https://fonts.googleapis.com/css?family=Josefin+Sans:400,300,600' rel='stylesheet' type='text/css'>
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/app.css">
+</head>
+<body>
+	<div class="ro-dash container-fluid">
+		<!-- Header bar of the dashboard -->
+		<header class="ro-main-header row">
+			<section class="ro-font-logo-container">
+				<a href=""><h1>E-commerce</h1></a>
+			</section>
+			<section class="options-container">
+					<div class="col-xs-6 ro-fade"></div>
+					<div class="ro-options-container">
+						<ul class="ro-options-nav">
+							<li><a href="#">Username</a></li>
+							<li><a href="#">Log out</a></li>
+						</ul>
+					</div>
+			</section>
+		</header>
+		<section class="ro-menu-container row">
+			<!-- Main Menu -->
+			<nav class="ro-main-menu">
+				<ul>
+					<li class="ro-header-menu"><h2>Menu</h2></li>
+					
+					<li class="ro-one-lvl">
+						<a class="ro-dropdown" href="#">
+							<i class="fa fa-desktop icon-before"></i>Menu 1<i class="fa fa-caret-right icon-after"></i>
+						</a>
+					</li>
+					
+					<li class="ro-one-lvl">
+						<a class="ro-dropdown" href="#">
+							<i class="fa fa-heart icon-before"></i>Menu 1<i class="fa fa-caret-right icon-after"></i>
+						</a>
+					</li>
+
+					<li class="ro-one-lvl">
+						<a class="ro-dropdown" href="#">
+							<i class="fa fa-graduation-cap icon-before"></i>Menu 1<i class="fa fa-caret-right icon-after"></i>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</section>
+	</div>
+	<!-- JS -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<script src="assets/js/jquery-2.1.4.min.js"></script>
+</body>
+</html>
